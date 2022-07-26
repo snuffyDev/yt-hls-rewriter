db64a515cf202c0e486b3dfcae6218733594d481
snuffy <72365477+snuffyDev@users.noreply.github.com>
2022-07-26 07:06:00 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,12 +2,42 @@ import { serve } from "https://deno.land/std@0.123.0/http/server.ts";
 
 const port = 10000;
 
-const handler = (request: Request): Response => {
-  let body = "Hello from Deno!\n\n";
-  body += "Your user-agent is:\n\n";
-  body += request.headers.get("user-agent") || "Unknown";
+const handler = async (request: Request): Response => {
+  const { pathname, searchParams, host } = new URL(request.url);
+  
+  if (pathname.startsWith("/api")) {
+    const _api = "manifest.googlevideo.com";
+    console.log(request.url.replace(host, _api))
+    request = new Request(request.url.replace(host, _api), request);
+    request.headers.set('Origin',"https://"+ _api);
+  
+    let response = await fetch(request);
+    response = new Response(response.body, response);
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.append('Vary', 'Origin');
+   
+    let body = await(await response.clone()).text();
+    body = body.replace(/https:\/\/(.*?)\//gm,'/')
+    const regex = /(\/(?:api|videoplayback)(?:.*?))(?=\n|")/gm;
+    body = body.replace(regex, "$1?host="+searchParams.get('host')); 
+    response = new Response(body, response)
+    return response;
+  
+  }
 
-  return new Response(body, { status: 200 });
+  if (pathname.startsWith("/videoplayback")) {
+    const _api = searchParams.get("host");
+    
+    request = new Request(request.url.replace(host, _api), request);
+    request.headers.set('Origin',"https://"+ _api);
+  
+    let response = await fetch(request);
+    response = new Response(response.body, response);
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.append('Vary', 'Origin');
+    response = new Response(response.body, response)
+    return response;
+  }
 };
 
 console.log(`HTTP webserver running on port ${port}.`);
