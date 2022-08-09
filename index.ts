fb61cfb671fddcfa82eb85d199d7cada18836b64
snuffy <72365477+snuffyDev@users.noreply.github.com>
2022-08-09 17:27:09 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,60 +1,68 @@
 import { serve } from "https://deno.land/std@0.123.0/http/server.ts";
 
-const port = 3000;
+const port = 3001;
 
 const handler = async (request: Request): Promise<Response> => {
-	const { pathname, searchParams, origin } = new URL(request.url);
+  const { pathname, searchParams, origin } = new URL(request.url);
+  const host = searchParams.get("host") || "";
+  try {
+    if (pathname.startsWith("/api")) {
+      const _api = "manifest.googlevideo.com";
 
-	if (pathname.startsWith("/api")) {
-		const _api = "manifest.googlevideo.com";
+      request = new Request(
+        request.url.replace(origin, "https://" + _api),
+        request,
+      );
+      request.headers.set("Origin", "https://" + _api);
 
-		request = new Request(request.url.replace(origin, 'https://' + _api), request);
-		request.headers.set('Origin', "https://" + _api);
+      let response = await fetch(request);
 
-		let response = await fetch(request);
+      response = new Response(response.body, response);
+      response.headers.set("Access-Control-Allow-Origin", "*");
+      response.headers.append("Vary", "Origin");
 
-		response = new Response(response.body, response);
-		response.headers.set('Access-Control-Allow-Origin', "*");
-		response.headers.append('Vary', 'Origin');
+      let body = await response.text();
+      // body = body.replace(/(https:\/\/.*?)(\/.*?)(?="|$)/, )
+      body = body.replace(/https:\/\/(.*?)\//gm, "/");
 
-		let body = await (await response.clone()).text();
-		body = body.replace(/https:\/\/(.*?)\//gm, "/");
+      const regex = /(\/(?:api|videoplayback)(?:.*?))(?=\n|")/gm;
 
-		const regex = /(\/(?:api|videoplayback)(?:.*?))(?=\n|")/gm;
+      body = body.replace(regex, "$1?host=" + searchParams.get("host"));
+      if (body.match(/(#EXT-X-MAP:URI=".*?)/gm)) {
+        body = body.replace(/(#EXT-X-MAP:URI=".*?)(?:\?)(host=.*")/gm, "$1&$2");
+      }
+      response = new Response(body, response);
+      response.headers.set("Access-Control-Allow-Origin", "*");
+      response.headers.append("Vary", "Origin");
 
-		body = body.replace(regex, "$1?host=" + searchParams.get('host'));
-		if (body.match(/(#EXT-X-MAP:URI=".*?)/gm)) {
+      return response;
+    }
 
-			body = body.replace(/(#EXT-X-MAP:URI=".*?)(?:\?)(host=.*")/gm, "$1&$2");
-			// console.log(body,);
-		}
-		response = new Response(body, response);
-		response.headers.set('Access-Control-Allow-Origin', "*");
-		response.headers.append('Vary', 'Origin');
+    const _api = searchParams.get("host") as string;
 
-		return response;
+    request = new Request(
+      request.url.replace(origin, "https://" + _api),
+      request,
+    );
+    request.headers.set("Origin", "https://" + _api);
 
-	}
+    let response = await fetch(request);
 
+    response = new Response(response.body, response);
 
-	const _api = searchParams.get("host") as string;
+    response.headers.set("Access-Control-Allow-Origin", "*");
+    response.headers.append("Vary", "Origin");
 
-	request = new Request(request.url.replace(origin, 'https://' + _api), request);
-	request.headers.set('Origin', "https://" + _api);
-
-	let response = await fetch(request);
-	const { readable, writable } = new TransformStream();
-	response = new Response(response.body, response);
-
-	response.headers.set('Access-Control-Allow-Origin', '*');
-	response.headers.append('Vary', 'Origin');
-	if (response.body) {
-
-		response.body.pipeTo(writable);
-		return new Response(readable, response);
-	}
-	return response;
+    return response;
+  } catch (error) {
+    console.error(error);
+    return new Response(error, {
+      status: 500,
+      statusText: error,
+      headers: { "Access-Control-Allow-Origin": "*" },
+    });
+  }
 };
 
 console.log(`HTTP webserver running on port ${port}.`);
-await serve(handler, { hostname: "0.0.0.0", port: port, });
+serve(handler, { hostname: "0.0.0.0", port: port });
